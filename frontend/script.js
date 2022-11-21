59c8ae3f78b520ad3969d3d834e8d0a72248f96e
bhavaniravi <bhava0895@gmail.com>
2022-11-21 11:25:06 +0530

    Fix calender issues
    1. Bad last date, new Date(currYear, currMonth+1, 0).getDate()` will give you the right one
    2. December was not re-rendered, something wrong with calculation
    3. use document.createElement("li") to render element instead of plain text
    4. This will help setting onclick onrerendering, previously it was not getting set when re-rendered

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,15 @@
-var elements = document.getElementsByClassName("dayTile");
+
+
+const daysTag = document.querySelector(".day-grid"),
+currentDate = document.querySelector(".current-date"),
+prevNextIcon = document.querySelectorAll("#prev, #next");
+
+let date = new Date(),
+currYear = date.getFullYear(),
+currMonth = date.getMonth();
+
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 var setMarked = function(e) {
     if (e.target.classList.contains("clicked")) 
@@ -6,6 +17,44 @@ var setMarked = function(e) {
     else (e.target.classList.add("clicked"));
 };
 
-for (var i = 0; i < elements.length; i++) {
-    elements[i].addEventListener('click', setMarked);
+const renderCalendar = () => {
+    lastDateofMonth = new Date(currYear, currMonth+1, 0).getDate(); // getting last date of month
+    daysTag.innerHTML = '';
+    for (let i = 0; i < lastDateofMonth; i++) { 
+        let style = "";
+        let date = new Date(currYear, currMonth, i+1)
+        let isToday = i === date.getDate() && currMonth === date.getMonth() && currYear === date.getFullYear() ? "active" : "";
+        
+        if (i == 0) style= `grid-column-start: ${date.getDay() + 1}`;
+        
+        li = document.createElement("li")
+        li.innerHTML = i + 1
+        li.style = style
+        li.className = `${isToday} dayTile`
+        li.onclick = setMarked
+
+        daysTag.appendChild(li)
+    }
+    document.getElementById("currentMonthYear").innerHTML = `${months[currMonth]} ${currYear}`;
 }
+
+
+
+prevNextIcon.forEach(icon => { // getting prev and next icons
+    icon.addEventListener("click", () => { // adding click event on both icons
+        // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
+
+        currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+        if(currMonth < 0 || currMonth > 11) { // if current month is less than 0 or greater than 11
+            // creating a new date of current year & month and pass it as date value
+            date = new Date(currYear, currMonth);
+            currYear = date.getFullYear(); // updating current year with new date year
+            currMonth = date.getMonth(); // updating current month with new date month
+        } else {
+            date = new Date(); // pass the current date as date value
+        }
+        renderCalendar(); // calling renderCalendar function
+    });
+});
+
+renderCalendar();
